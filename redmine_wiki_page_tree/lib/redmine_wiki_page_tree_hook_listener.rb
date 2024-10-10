ae2ab79eeb7385cbe757092f72ea9a50615b27bb
Chance <ahs0432@naver.com>
2024-10-10 14:07:09 +0900

    🔥 기본 페이지 보기를 닫기로 반영 후 현재 페이지의 상하위 페이지만 열리도록 변경

--- redmine_wiki_page_tree/lib/redmine_wiki_page_tree_hook_listener.rb
+++ redmine_wiki_page_tree/lib/redmine_wiki_page_tree_hook_listener.rb
@@ -2,7 +2,7 @@ class RedmineWikiPageTreeHookListener < Redmine::Hook::ViewListener
   include ActionView::Helpers::DateHelper
   include ActionView::Context
 
-  # サイドバーにページツリーを表示
+  # 사이드바 제작
   def view_layouts_base_sidebar(context = {})
     return unless context[:controller]
     params = context[:controller].params
@@ -10,14 +10,15 @@ class RedmineWikiPageTreeHookListener < Redmine::Hook::ViewListener
 
     pages = load_pages(context[:request].params[:project_id])
     pages_by_parent_id = pages.group_by(&:parent_id)
+    page_arr = check_my_page(pages, params[:id])
 
     content_tag(:div, class: 'page-tree') do
       concat content_tag(:h3, l(:page_tree), class: 'page-tree__title')
-      concat render_page_hierarchy(pages_by_parent_id, nil, :timestamp => true)
+      concat render_page_hierarchy(pages_by_parent_id, nil, {:timestamp => true, :id => page_arr})
     end
   end
 
-  # assetsの追加
+  # Asset 파일 불러오기
   def view_layouts_base_html_head(context)
     return unless context[:controller]
     params = context[:controller].params
@@ -31,6 +32,7 @@ class RedmineWikiPageTreeHookListener < Redmine::Hook::ViewListener
 
   private
 
+  # 페이지 목록 불러와 Array 형태로 전달
   def load_pages(project_id)
     Project.find(project_id)
       .wiki
@@ -42,13 +44,27 @@ class RedmineWikiPageTreeHookListener < Redmine::Hook::ViewListener
       .to_a
   end
 
+  # 사이드바 제작
   def render_page_hierarchy(pages, node=nil, options={})
     content = ''
     if pages[node]
       content << "<ul class=\"page-tree__list\">\n"
+
       pages[node].each do |page|
         if pages[page.id]
-          content << "<details open>"
+          id_check = false
+          options[:id].each do |page_id|
+            if page_id == page.id.to_s
+              id_check = true
+            end
+          end
+
+          if id_check
+            content << "<details open>"
+          else
+            content << "<details>"   
+          end
+
           content << '<summary class="page-tree__node">'
           content << link_to_wiki(page, options)
           content << "</summary>\n"
@@ -67,10 +83,32 @@ class RedmineWikiPageTreeHookListener < Redmine::Hook::ViewListener
     content.html_safe
   end
 
+  # 현재 페이지의 상위 페이지 리스팅 후 전달
+  def check_my_page(pages, page_id)
+    page_parent = Hash.new{}
+    pages.each do |page|
+      page_parent[page.id.to_s] = page.parent_id.to_s
+    end
+
+    page_arr = [page_id]
+
+    while true
+      if page_parent[page_id] == ""
+        break
+      else
+        page_arr.append(page_parent[page_id])
+        page_id = page_parent[page_id]
+      end
+    end
+
+    page_arr
+  end
+
+  # 위치 관련 정보 전달
   def link_to_wiki(page, options)
     link_to(
       h(page.pretty_title),
-      {:controller => 'wiki', :action => 'show', :project_id => page.project, :id => page.title, :version => nil},
+      {:controller => 'wiki', :action => 'show', :project_id => page.project, :id => page.id, :version => nil},
       :title => (options[:timestamp] && page.updated_on ? l(:label_updated_time, distance_of_time_in_words(Time.now, page.updated_on)) : nil),
       :draggable => 'true',
       :data => { :wiki_page_id => page.id } )
